7c2f46bba610b1d5f5d4e8082d32d6e157099abf
BXS-Archipelago <bxs@tutanota.com>
2021-06-03 13:51:59 +0000

    create homes.html

--- app.py
+++ app.py
@@ -16,10 +16,15 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 
+# an instance of pymongo by adding a constructor method, so the app object communicates with the Mongo db. 
+mongo =PyMongo(app)
+
+
+
 @app.route("/")
 @app.route("/get_homes")
 def get_homes():
-    homes = list(mongo.db.homes.find())
+    
     return render_template("homes.html", homes=homes)
 
 
@@ -33,5 +38,5 @@ def get_homes():
 
 
     if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")),
-    debug=True)
+        app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")),
+        debug=True)
