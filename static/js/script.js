2677d07ade2db934f17673d372978a539abf06fa
BXS-Archipelago <bxs@tutanota.com>
2021-06-11 17:56:43 +0000

    create JS for Modal view_home page

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,10 @@
+document.addEventListener("click", function (e){
+    if(e.target.classList.contains("gallery-item")){
 
-
-// var numberOfItems = $('#loop .card').length
-// var limitPerPage = 4;
-// $("#loop .card:gt(" + (limitPerPage  - 1) +")").hide();
-// var totalPages = Math.round(numberOfItems / limitPerPage)
-// $('.pagination').append("<li class='current-page page-item active'><a class='page-link' href='javascript:void()'> " + 1 + "</a></li>");
-
-// for(var i = 2; i <= totalPages; i++) {
-//     $(".pagination").append("<li class = 'current-page' class='page-item'><a class='page-link' href='javascript:void()'> " + i + "</a></li>");
-// }
-// $(".pagination").append("<li class='page-item'><a class='page-link' href='javascript:void(0)'>Next</a></li>");
-
-// $(".pagination li.current-page").on("click", function() {
-//     if($(this).hasClass("active")){
-//         return false;
-//     } else {
-//         var currentPage = $(this).index();
-//         $(".pagination li").removeClass('active');
-//         $(this).addClass("active");
-//         $("#loop .card").hide();
-
-//         var sumTotal = limitPerPage * currentPage;
-//         for (var i = sumTotal - limitPerPage; i < sumTotal; i++) {
-//             $("#loop .card:eq(" + i +")").show();
-//         }
-//     }
-// });
-
-
-
+        const src = e.target.getAttribute("src");
+        document.querySelectorAll("modal-img").src = src;
+        var myModal = new bootstrap.Modal(document.getElementById("gallery-modal"));
+        myModal.show();
+    }
+})
+   
